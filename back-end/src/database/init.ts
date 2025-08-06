47eb32c313b6314515e69d211fecd6f2a6bad416
Camren Daly <camren.daly@globant.com>
2025-08-05 17:34:52 -0700

    fixing litestream config

--- back-end/src/database/init.ts
+++ back-end/src/database/init.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import fs from 'fs';
 
 // Database directory
-const dbDir = path.resolve(__dirname, '../../TEMP/data/app/data');
+const dbDir = path.resolve(__dirname, '../../TEMP/app/data');
 // Database file path
 const dbPath = path.resolve(dbDir, 'onlyfans.db');
 
